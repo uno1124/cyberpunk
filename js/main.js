9624d6eadfca88030e000f365da59a71587ce5e3
uno1124 <aqoqaq@gmail.com>
2024-11-07 23:36:39 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+  })
